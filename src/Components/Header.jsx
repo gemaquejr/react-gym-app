89617f1c1f632f91a4abc44403ff9f5f46d0be3c
Francisco Gemaque <gemaquejr@hotmail.com>
2025-10-18 17:19:12 -0300

    Adiciona componente Header
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+  <div id="main">
+    <div className="header-heading">
+        <h2>FORTALEÇA SEU</h2>
+        <h1><span>CORPO E SUA SAÚDE</span> CONOSCO</h1>
+        <p className="details">Construa seu corpo e condicionamento físico com toque profissional</p>
+        <div className="header-btns">
+            <a href="#" className="header-btn">VAMOS LÁ</a>
+        </div>
+    </div>
+  </div>
+  )
+}
+
+export default Header;
