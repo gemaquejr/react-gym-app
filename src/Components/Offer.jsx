0d7e4c00396d2b2e92e2811dcb38b6b4e28b296f
Francisco Gemaque <gemaquejr@hotmail.com>
2025-10-18 19:28:13 -0300

    Adiciona componente Offer
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

---
+++ src/Components/Offer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Offer() {
+    return (
+        <div id="offer">
+            <div className="pr-heading">
+                <h1>UMA GRANDE <span>PROMOÇÃO</span> PARA O INÍCIO DO VERÃO</h1>
+                <p className="details">Que tal uma promoção imperdível para voltar a treinar ainda neste mês?</p>
+                <div className="pr-btns">
+                    <a href="#" className="pr-btn">CONFIRA</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Offer;
