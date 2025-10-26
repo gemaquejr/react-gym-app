0a8b0a90b851c4638a1fa71bb5e3a43b8251f51f
Francisco Gemaque <gemaquejr@hotmail.com>
2025-10-25 21:04:26 -0300

    Fix: refatora o componente Offer
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/Components/Offer.jsx
+++ src/Components/Offer.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Button from "./Button";
 
 function Offer() {
     return (
-        <div id="offer">
+        <section id="offer">
             <div className="pr-heading">
                 <h1>UMA GRANDE <span>PROMOÇÃO</span> PARA O INÍCIO DO VERÃO</h1>
                 <p className="details">Que tal uma promoção imperdível para voltar a treinar ainda neste mês?</p>
                 <div className="pr-btns">
-                    <a href="/#" className="pr-btn">CONFIRA</a>
+                    <Button type="submit" >
+                        {"Confira"}
+                    </Button>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
