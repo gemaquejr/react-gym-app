b8bc950939297ca5388fa110341ade7ba518afae
Francisco Gemaque <gemaquejr@hotmail.com>
2025-10-25 21:02:25 -0300

    Fix: refatora o componente Header
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,25 @@
 import React from "react";
+import Button from "./Button";
 
 function Header() {
   return (
-  <div id="main">
-    <div className="header-heading">
+    <header id="main">
+      <div className="header-heading">
         <h2>FORTALEÇA SEU</h2>
-        <h1><span>CORPO E SUA SAÚDE</span> CONOSCO</h1>
-        <p className="details">Construa seu corpo e condicionamento físico com toque profissional</p>
+        <h1>
+          <span>CORPO E SUA SAÚDE</span> CONOSCO
+        </h1>
+        <p className="details">
+          Construa seu corpo e condicionamento físico com toque profissional
+        </p>
         <div className="header-btns">
-            <a href="/#" className="header-btn">VAMOS LÁ</a>
+          <Button type="submit">
+            {"começar"}
+          </Button>
         </div>
-    </div>
-  </div>
-  )
+      </div>
+    </header>
+  );
 }
 
 export default Header;
