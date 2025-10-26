85e8b92a811f97c6fc3e88608514cd440a1dc4b2
Francisco Gemaque <gemaquejr@hotmail.com>
2025-10-25 21:03:04 -0300

    Fix: refatora o componente Feature
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -7,15 +7,15 @@ import fimage4 from '../images/4.svg'
 
 function Feature() {
     return (
-        <div id="features">
+        <section id="features">
             <h1>PROGRAMAS</h1>
             <div className="a-container">
-                <Featurebox image={fimage1} title="MUSCULAÇÃO" />
-                <Featurebox image={fimage2} title="TREINO FUNCIONAL" />
-                <Featurebox image={fimage3} title="HIPERTROFIA" />
-                <Featurebox image={fimage4} title="CARDIO" />
+                <Featurebox image={fimage1} title="MUSCULAÇÃO">Aumente mobilidade, resistência e performance do dia a dia.</Featurebox>
+                <Featurebox image={fimage2} title="TREINO FUNCIONAL">Aumente mobilidade, resistência e performance do dia a dia.</Featurebox>
+                <Featurebox image={fimage3} title="HIPERTROFIA">Planos específicos para hipertrofia com acompanhamento.</Featurebox>
+                <Featurebox image={fimage4} title="CARDIO">Sessões para condicionamento e queima calórica eficiente.</Featurebox>
             </div>
-        </div>
+        </section>
     )
 }
 
